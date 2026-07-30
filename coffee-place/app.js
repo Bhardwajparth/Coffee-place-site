41bebc982a0a3935b3590d557176331916f7b165
Parth Bhardwaj <19bhardwajparth@gmail.com>
2026-07-30 18:43:05 +0530

    fixing form

--- coffee-place/app.js
+++ coffee-place/app.js
@@ -1,7 +1,8 @@
 const navLinks = document.querySelectorAll(".nav-menu .nav-link")
 const menuOpenBtn = document.querySelector("#menu-open-button")
 const menuCloseBtn = document.querySelector("#menu-close-button")
-
+// const Submit = document.querySelector(".submit-button")
+const formContact = document.querySelectorAll(".contact-form")
 
 menuOpenBtn.addEventListener("click" , () => {
     // Toggle mobile menu visibility
@@ -48,3 +49,7 @@ const swiper = new Swiper('.slider-wrapper', {
     }
   }
 });
+
+formContact.addEventListener("submit", () => {
+  formContact.reset();
+})
